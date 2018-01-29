ffa17f2267b4b1f0646f7652cff981d846c881ab
Eden Mesfun <emesfun@gmail.com>
2018-01-29 13:57:28 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2,  "oil paintings" => 3}
+  gardening_tools = {"whale bone corsets" => 5, "porcelain vases" => 2,  "oil paintings" => 3}
 end
 
 
