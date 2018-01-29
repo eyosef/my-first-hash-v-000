674cdaea8ddcc101d4b8bd4df61bf0719dd0eb15
Eden Mesfun <emesfun@gmail.com>
2018-01-29 13:57:58 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  gardening_tools = {"whale bone corsets" => 5, "porcelain vases" => 2,  "oil paintings" => 3}
+  gardening_tools = {"" => "seed packet", "porcelain vases" => 2,  "oil paintings" => 3}
 end
 
 
